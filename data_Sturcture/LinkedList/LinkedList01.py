e63371eee8db01aa8e608f6cf943f960b6484b6b
ryudongjae <rdj10149@gmail.com>
2021-03-21 01:59:17 +0900

    LinkedList

---
+++ data_Sturcture/LinkedList/LinkedList01.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class NodeMgmt:
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def add(self, data):
+        if self.head == '':
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
+
+    def delete(self,data):
+        if self.head =='':
+            print("해당 값을 가진 노드가 없습니다.")
+            return
+        if self.head.data ==data:
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                    return
+                else:
+                    node = node.next
+
+linkedlist1 = NodeMgmt(0)
+print(linkedlist1.desc())
+print(linkedlist1.head)
+print(linkedlist1.delete(0))
+print(linkedlist1.desc())
+linkedlist1= NodeMgmt(0)
+print(linkedlist1.desc())
+for data in range(1,10):
+    linkedlist1.add(data)
+    print(linkedlist1.desc())
+    print(linkedlist1.delete(4))
+    print(linkedlist1.delete(0))
